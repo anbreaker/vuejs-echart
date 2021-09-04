5912a9bbb1cc3ebebcdb6bac67d8a07f3c219412
anbreaker <antunez19@gmail.com>
2021-09-04 08:24:20 +0200

    firts chart

---
+++ echart/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Pie />
+  </div>
+</template>
+
+<script>
+  import Pie from './components/Pie.vue';
+
+  export default {
+    name: 'App',
+
+    components: {
+      Pie,
+    },
+  };
+</script>
+
+<style>
+  body {
+    background-color: #100c2a;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ echart/src/components/Pie.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="chart">
+    <v-chart class="chart" :option="option" />
+  </div>
+</template>
+
+<script>
+  import { use } from 'echarts/core';
+  import { CanvasRenderer } from 'echarts/renderers';
+  import { PieChart } from 'echarts/charts';
+  import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components';
+  import VChart, { THEME_KEY } from 'vue-echarts';
+
+  use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent]);
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      VChart,
+    },
+    provide: {
+      [THEME_KEY]: 'dark',
+    },
+    data() {
+      return {
+        option: {
+          tooltip: { trigger: 'item' },
+          legend: {
+            show: true,
+            top: '5%',
+            left: 'left',
+            orient: 'vertical',
+            formatter: (name) => {
+              return name + '\n1048';
+            },
+          },
+          series: [
+            {
+              name: 'Alarmas',
+              type: 'pie',
+              radius: ['40%', '70%'],
+              avoidLabelOverlap: true,
+              label: {
+                show: true,
+                position: 'center',
+                fontSize: '40',
+                formatter: () => {
+                  return 'Total\n10\nalarms';
+                },
+              },
+              labelLine: { show: false },
+              data: [
+                { value: 1048, name: 'Acknowledge' },
+                { value: 735, name: 'Unacknowledge' },
+              ],
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .chart {
+    height: 400px;
+  }
+</style>
